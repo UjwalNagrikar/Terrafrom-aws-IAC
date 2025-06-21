7a11a54cd0a900c6c3bd29615021ea27bd19ad0b
Ujwal Nagrikar <98095028+UjwalNagrikar@users.noreply.github.com>
2025-06-21 17:14:34 +0530

    Update main.tf

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -1,6 +1,10 @@
 resource "aws_instance" "myserver" {
   ami           = "ami-0f535a71b34f2d44a"
   instance_type = "t2.micro"
+
+  lifecycle {
+    prevent_destroy = true
+  }
   
   tags = {
     Name = "My-Server"
