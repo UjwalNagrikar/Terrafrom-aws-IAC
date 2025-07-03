191fa68054adbd4af6cd97f5a344c6244e0c0dac
Ujwal Nagrikar <98095028+UjwalNagrikar@users.noreply.github.com>
2025-07-03 19:55:51 +0530

    Update main.tf

--- aws-vpc/main.tf
+++ aws-vpc/main.tf
@@ -1,25 +1,13 @@
-#EC2 create
-resource "aws_instance" "Ujwal-Server" {
-    ami = "ami-0f585a71d34f2d32b"
-    instance_type = "t2.micro"
-    key_name = "Ujwal-SRE"
-    subnet_id = aws_subnet.public-subnet-1.id
-    associate_public_ip_address = true 
-    tags = {
-      Name = "Ujwal-Server"
-    }
-}
-
-#Vpc create
+# VPC
 resource "aws_vpc" "Ujwal-VPC" {
-    cidr_block = "10.0.0.0/16"
-    instance_tenancy = "default"
-    tags = {
-      Name = "Ujwal-VPC"
-    }
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "Ujwal-VPC"
+  }
 }
 
-#Internet getway 
+# Internet Gateway (single definition)
 resource "aws_internet_gateway" "Ujwal-IGW" {
   vpc_id = aws_vpc.Ujwal-VPC.id
   tags = {
@@ -27,33 +15,25 @@ resource "aws_internet_gateway" "Ujwal-IGW" {
   }
 }
 
-#Subnet 1
+# Public Subnet
 resource "aws_subnet" "public-subnet-1" {
-  vpc_id = aws_vpc.Ujwal-VPC.id
+  vpc_id     = aws_vpc.Ujwal-VPC.id
   cidr_block = "10.0.0.0/24"
   tags = {
     Name = "public-subnet-1"
   }
 }
 
-#subnet 2 
+# Private Subnet
 resource "aws_subnet" "privet-subnet-2" {
-  vpc_id = aws_vpc.Ujwal-VPC.id
+  vpc_id     = aws_vpc.Ujwal-VPC.id
   cidr_block = "10.0.2.0/24"
   tags = {
     Name = "privet-subnet"
   }
 }
 
-# internet getway 
-resource "aws_internet_gateway" "Ujwal-IGW" {
-  vpc_id = aws_vpc.Ujwal-VPC.id
-  tags = {
-    Name = "Ujwal-IGW"
-  }
-}
-
-#Route Table 
+# Route Table
 resource "aws_route_table" "my-route" {
   vpc_id = aws_vpc.Ujwal-VPC.id
 
@@ -67,8 +47,20 @@ resource "aws_route_table" "my-route" {
   }
 }
 
-# Route table association 
+# Route Table Association
 resource "aws_route_table_association" "route-association" {
-  subnet_id = aws_subnet.public-subnet-1.id
+  subnet_id      = aws_subnet.public-subnet-1.id
   route_table_id = aws_route_table.my-route.id
+}
+
+# EC2 Instance
+resource "aws_instance" "Ujwal-Server" {
+  ami                         = "ami-0f585a71d34f2d32b"
+  instance_type               = "t2.micro"
+  key_name                    = "Ujwal-SRE"
+  subnet_id                   = aws_subnet.public-subnet-1.id
+  associate_public_ip_address = true
+  tags = {
+    Name = "Ujwal-Server"
+  }
 }
