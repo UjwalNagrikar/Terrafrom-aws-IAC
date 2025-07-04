f2ac1272504c5cb68d1f29098a5cf322f899bcd0
Ujwal Nagrikar <98095028+UjwalNagrikar@users.noreply.github.com>
2025-07-04 18:54:43 +0530

    Update main.tf

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "myserver" {
   ami           = "ami-0f535a71b34f2d44a"
   instance_type = "t2.micro"
-  associate_public_ip_address = true
 
   lifecycle {
     prevent_destroy = true
